11b9f64415f6f0d2aade361b9c6fd2541fe7e26f
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-04-02 14:07:33 -0600

    created apartment model

--- apartment_management_project_120919/db/schema.rb
+++ apartment_management_project_120919/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_200350) do
+ActiveRecord::Schema.define(version: 2020_04_02_200709) do
+
+  create_table "apartments", force: :cascade do |t|
+    t.string "unit"
+    t.integer "bulding_id"
+    t.integer "max_occupants"
+    t.float "rent"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "buildings", force: :cascade do |t|
     t.string "name"
