03ba769167466ef0b77f9a140a9211fec7715d53
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-04-03 19:45:06 -0500

    update

--- apartment_management_project_120919/config/initializers/devise.rb
+++ apartment_management_project_120919/config/initializers/devise.rb
@@ -3,6 +3,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # config.omniauth :facebook, App ID(228074975226315), App Secret(c43d4f6ecfdbd9b1a4b2d51ad2bb1ef7),
+  # callback_url: "http://localhost:3000/auth/facebook/callback"
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
